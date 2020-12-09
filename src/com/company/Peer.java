77d827c4dcfcc473077becad4b3d7632827794d9
DivyathaSatheeshEdachana <divyathasatheeshdivz@gmail.com>
2020-12-09 18:45:15 -0500

    Added updatedownloadingrate for selection of preferred peers.

--- src/com/company/Peer.java
+++ src/com/company/Peer.java
@@ -11,7 +11,6 @@ public class Peer {
     private int peerId;
     private Socket connection;
     private byte[] bitfield;
-    Map<Integer, Integer> PeerIDtoDownloadRate;  //initialize this somewhere
 
     Boolean unchoked = false; // maintain current status of that Neighbour
     // getters
@@ -33,10 +32,6 @@ public class Peer {
 
     public byte[] getBitfield(){
         return this.bitfield;
-    }
-
-    public Map<Integer, Integer> getPeerIDtoDownloadRate(){
-        return this.PeerIDtoDownloadRate;
     }
 
     // setters
