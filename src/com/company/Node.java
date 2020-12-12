ac1a1481dcaa4294e8d44f7e0c913422ba3d0b6f
Avanti-k <avanti.199@gmail.com>
2020-12-12 01:47:59 -0500

    Check if finish bug fix

--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -51,7 +51,7 @@ public class Node extends Thread{
     MyFileHandler myFileHandler;
     int selfId;
     LoggerModule logger = new LoggerModule();
-
+    Lock donePeerLock;
     public Node(int selfId)
     {
         unchokedPeers = new HashSet<>();
@@ -67,6 +67,7 @@ public class Node extends Thread{
         sPort = peerInfoHandler.getPeerHashMap().get(selfId).getPortNo();
         selfPeer = peerInfoHandler.getPeerHashMap().get(selfId);
         donePeers = new HashSet<>();
+        donePeerLock = new ReentrantLock();
 
     }
 
@@ -196,12 +197,17 @@ public class Node extends Thread{
     }
 
     synchronized public void updateDoneSet( int peerId){
+        donePeerLock.lock();
         donePeers.add(peerId);
-        if(donePeers.size() == peerInfoHandler.getPeerHashMap().size()){
+        System.out.println(selfId + " : got updateDoneSet size =  " + donePeers.size());
+        if(donePeers.size() == peerInfoHandler.getPeerHashMap().size() - 1){
             // TODO terminate program here
             System.out.println("\n ******** " + currentThread().getName() + " :  ********** All peers completed ********* \n");
+            donePeerLock.unlock();
             System.exit(0);
         }
+        donePeerLock.unlock();
+
     }
     public void sendHavePieceUpdateToAll(int pieceIndex){
         PeerHandler[] handlersSet = PeerMap.values().toArray(new PeerHandler[0]);
